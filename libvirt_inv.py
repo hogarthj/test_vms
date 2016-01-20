ff09b1c5213ee98cfabe4d476eb11d1eeb750a50
James Hogarth <james.hogarth@gmail.com>
2016-01-20 16:30:38 +0000

    automatically pick a primary for postgres cluster and use linvirt API for dynamic inventory

--- libvirt_inv.py
+++ libvirt_inv.py
@@ -7,11 +7,7 @@ Dynamic inventory of libvirt script for Ansible, in Python.
 import sys
 import argparse
 import libvirt
-
-try:
-    import json
-except ImportError:
-    import simplejson as json
+import json
 
 class LibvirtInventory(object):
 
@@ -25,53 +21,57 @@ class LibvirtInventory(object):
 
         # Called with `--list`.
         if self.args.list:
-            self.inventory = self.get_inv()
+            self.get_inv()
         # Called with `--host [hostname]`.
         elif self.args.host:
-            self.inventory = self.inventory
+            #self.dom_info(self.args.host)
+            pass
 
         print json.dumps(self.inventory);
 
     def get_inv(self):
-
         domains = self.conn.listDomainsID()
         if len(domains) == 0:
-            return self.inventory
+            return
         else:
-            return self.get_domains(domains)
+            for domain in domains:
+                self.dom_info(domain)
 
 
-    def get_domains(self, domains):
-        for dom in domains:
-            domain = self.conn.lookupByID(dom)
+    def dom_info(self, dom):
+        if isinstance(dom, int):
+          domain = self.conn.lookupByID(dom)
+        else:
+            domain = self.conn.lookupByName(dom)
+        try:
+            dom_inv = json.loads(domain.metadata(0, None))
+        except (ValueError,libvirt.libvirtError):
+            dom_inv = {}
+        finally:
             try:
-                dom_inv = json.loads(domain.metadata(0, None))
-            except (ValueError,libvirt.libvirtError):
-                dom_inv = {}
-            finally:
+                dom_host_vars = {}
+                dom_ifaces = domain.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT)
+                if dom_ifaces != None:
+                    for iface in dom_ifaces:
+                        if iface == 'lo':
+                            continue
+                        for addr in dom_ifaces[iface]['addrs']:
+                            if addr['type'] == 0:
+                                dom_host_vars['ansible_ssh_host'] =  addr['addr']
+                if 'ansible_ssh_host' not in dom_host_vars:
+                    return
                 if 'groups' in dom_inv:
                     for group in dom_inv['groups']:
                         if group in self.inventory:
                             self.inventory[group]['hosts'].append(domain.name())
                         else:
                             self.inventory.update({group: {'hosts': [domain.name()]}})
-                dom_host_vars = {}
                 dom_host_vars['ansible_ssh_user'] = 'root'
-                try:
-                    dom_ifaces = domain.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT)
-                    if dom_ifaces != None:
-                        for iface in dom_ifaces:
-                            if iface == 'lo':
-                                continue
-                            for addr in dom_ifaces[iface]['addrs']:
-                                if addr['type'] == 0:
-                                    dom_host_vars['ansible_ssh_host'] =  addr['addr']
-                except (TypeError,libvirt.libvirtError):
-                    pass
                 if 'hostvars' in dom_inv:
                     dom_host_vars.update(dom_inv['hostvars'])
                 self.inventory['_meta']['hostvars'].update({domain.name(): dom_host_vars })
-        return self.inventory
+            except (TypeError,libvirt.libvirtError):
+                pass
 
     # Read the command line args passed to the script.
     def read_cli_args(self):
