06ddc91706375cc81214b5862b4c0a61450b918a
James Hogarth <james.hogarth@gmail.com>
2016-07-15 16:27:19 +0100

    add NextCLoud testing scripts and role

---
+++ gen_nc_vms.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "" ]]
+then
+    dist=$1
+else
+    dist="f24"
+fi
+
+for web in httpd nginx
+  do 
+    for db in mysql postgres
+      do 
+        if [[ ${dist} =~ f[[:digit:]] ]]
+        then
+            groups="\"fedora\", \"nextcloud\", \"${web}\", \"${db}\""
+        else
+            groups="\"nextcloud\", \"${web}\", \"${db}\""
+        fi
+        vmclone ${dist}-nc-${web}-${db}
+        virsh desc ${dist}-nc-${web}-${db} "{\"groups\": [${groups}]}"
+        virsh start ${dist}-nc-${web}-${db}
+    done
+done 
---
+++ kill_nc_vms.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "" ]]
+then
+    dist=$1
+else
+    dist="f24"
+fi
+
+vmpat="${dist}-nc-"
+
+virsh list --all | awk -v vmpat=${vmpat} '$2 ~ vmpat {print $2}'| while read line; do vmkill $line ; done
